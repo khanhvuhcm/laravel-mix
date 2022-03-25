717d80d452c265eb2b3fa855c2140a2dabeff9f5
Vu Hong Khanh <khanh.vuhong@nashtechglobal.com>
2022-03-25 10:11:29 +0700

    1

---
+++ webpack.mix.js
@@ -0,0 +1,31 @@
+const mix = require('laravel-mix');
+const chokidar = require('chokidar');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        //
+    ]);
+mix.webpackConfig({
+    devServer: {
+        host: "0.0.0.0",
+        port: 8080,
+        onAfterSetupMiddleware(server) {
+            chokidar.watch([
+                './resources/views/**/*.blade.php'
+            ], { ignoreInitial: true }).on('all', function() {
+                server.sendMessage(server.webSocketServer.clients, 'content-changed');
+            })
+         },
+    },
+});
